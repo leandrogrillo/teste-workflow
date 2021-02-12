2ef3584c07d72b415fe5b0c4858e594108e3e088
Leandro Grillo <leandrocesar.grillo@gmail.com>
2021-02-12 18:12:08 -0300

    feat: create project

---
+++ packages/module-a/lib/module-a.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = moduleA;
+
+function moduleA() {
+    console.log('module a')
+}
---
+++ packages/module-c/lib/module-c.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = moduleC;
+
+function moduleC() {
+    console.log('module c')
+}
